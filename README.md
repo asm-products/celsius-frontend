35e60298dd65f0105287f341adcc7928d6ee1c50
Rajat Talwar <cuterajat26@gmail.com>
2015-04-10 22:41:46 +0100

    added karma configuration,and changed bower/npm dependencies

--- README.md
+++ README.md
@@ -11,3 +11,13 @@ This is a product being built by the Assembly community. You can help push this 
 Assembly products are like open-source and made with contributions from the community. Assembly handles the boring stuff like hosting, support, financing, legal, etc. Once the product launches we collect the revenue and split the profits amongst the contributors.
 
 Visit [https://assembly.com](https://assembly.com) to learn more.
+
+#Setup
+
+npm install -g ionic
+
+npm install
+
+bower install
+
+ionic serve
---
+++ karma.config.js
@@ -0,0 +1,72 @@
+// Karma configuration
+// Generated on Tue Nov 11 2014 12:05:54 GMT+0530 (IST)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+
+      'www/lib/ionic/js/ionic.bundle.js',
+      'www/lib/angular-mocks/angular-mocks.js',
+      'www/app/**/*.js',
+        'www/js/ngCordova/ng-cordova.min.js',
+      'tests/*.js'
+    ],
+
+
+    // list of files to exclude
+    exclude: [
+        'www/app/app-init.js'
+
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['Chrome'],
+    //browsers: ['PhantomJS'],
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false
+  });
+};
