4be33e77e233bc1f4bcb946b3602b814ca96773e
Rajat Talwar <cuterajat26@gmail.com>
2015-04-22 02:37:31 +0530

    Added location info in the list item

--- www/js/app.js
+++ www/js/app.js
@@ -67,5 +67,5 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     }
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/discover');
 });
